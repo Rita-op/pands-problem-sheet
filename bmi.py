d2895e35dd86499d631c0e47d96e69fbbabce5a1
Rita Ortega <ritaortega@MacBook-Air-de-Rita.local>
2022-01-31 22:39:20 +0000

    Updated with weekly task 02 solution – attempt 2

--- bmi.py
+++ bmi.py
@@ -3,17 +3,21 @@
 
 # I found how the Body Mass Index (BMI) is calculated on this website: https://www.calculator.net/bmi-calculator.html
 
-
+# It reads the numbers given
 weight = input('Enter your weight (kg):')
 height = input('Enter your height (cm):')
-heightm = int(height)/100
 
-BMI = int(weight) / (heightm)**2
+# It converts the height from string to an integer and from centimeters to meters
+height_m = int(height)/100
 
+# It calculates the Body Mass Index with the given values
+BMI = int(weight) / (height_m)**2
+
+# It prints out a message with the result of the Body Mass Index calculation with two decimals
 bmiprint = 'The Body Mass Index (kg/m2) is {:.2f}'
 print(bmiprint.format(BMI))
 
-#REFERENCES : https://www.w3schools.com/python/python_string_formatting.asp
+# REFERENCES : https://www.w3schools.com/python/python_string_formatting.asp
 
 
 
