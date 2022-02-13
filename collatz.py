0f3f6b5cda6261f0dbb74775cc41946dbc9bf28a
Rita Ortega <ritaortega@MacBook-Air-de-Rita.local>
2022-02-13 23:40:03 +0000

    Updated with weekly task 04 solution – attempt 1

---
+++ collatz.py
@@ -0,0 +1,20 @@
+# The solution to weekly task 04: "Write a program that asks the user to input any positive integer and outputs the successive values of the given calculation"
+# Author: Rita Ortega
+
+
+values = []
+
+"""  
+REFERENCES:
+
+https://www.w3schools.com/python/python_while_loops.asp
+https://stackoverflow.com/questions/50012895/pythonwrite-a-program-that-keeps-reading-positive-numbers-from-the-user/50013009
+https://pynative.com/python-accept-list-input-from-user/
+https://www.edureka.co/blog/input-a-list-in-python/
+https://stackoverflow.com/questions/36843103/while-loop-with-if-else-statement-in-python/36843356
+
+
+
+"""
+
+
