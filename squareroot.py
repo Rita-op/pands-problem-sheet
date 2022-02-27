c4013394f794de5bbedf84e1d34a94bea2f797d0
Rita Ortega <ritaortega@MacBook-Air-de-Rita.local>
2022-02-27 23:33:15 +0000

    Updated with weekly task 06 solution – attempt 1

---
+++ squareroot.py
@@ -0,0 +1,16 @@
+# The solution to weekly task 06: "Write a program that takes a positive floating-point number as input and outputs an approximation of its square root"
+# Author: Rita Ortega
+
+
+
+
+"""
+
+REFERENCES:
+
+https://stackoverflow.com/questions/55232484/newtons-method-for-approximating-square-roots
+https://python.plainenglish.io/6-amazing-algorithms-to-get-the-square-root-and-any-root-of-any-number-in-python-3c976ad1ca04
+https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
+
+
+"""
