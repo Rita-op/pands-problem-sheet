1204bfde23f21ecf33a3bc752d59fe65d90be6bb
Rita Ortega <ritaortega@MacBook-Air-de-Rita.local>
2022-03-11 22:15:32 +0000

    Updated with weekly task 07 solution – attempt 2

--- es.py
+++ es.py
@@ -2,11 +2,26 @@
 # Author: Rita Ortega
 
 
+import fileinput
+countLetter = 0
+
+for line in fileinput.input(mode = "r"):
+    for letter in line:
+      if (letter == "e" or letter == "E"):
+       countLetter = countLetter + 1 
+print (countLetter)
+
+
+
+
 """
 
 REFERENCES:
 
+https://stackoverflow.com/questions/7033987/get-a-file-from-cli-input
+https://docs.python.org/3/library/fileinput.html
+https://www.codespeedy.com/count-occurrence-of-character-in-file-using-python/
+https://www.sanfoundry.com/python-program-read-file-counts-number/
 
- 
 
 """
